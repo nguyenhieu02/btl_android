7c65bbc7d2a47e85bfcd5020b0f1b8e0ac8635bd
nguyenhieu02 <nguyenvanhieu30122002@gmail.com>
2023-04-04 21:54:20 +0700

    android_appsalephone

---
+++ loginsignup/app/src/main/java/com/ssv/loginsignup/Class/SlidePhoto.java
@@ -0,0 +1,19 @@
+package com.ssv.loginsignup.Class;
+
+
+public class SlidePhoto {
+
+    private int resourceId;
+
+    public SlidePhoto(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+}
